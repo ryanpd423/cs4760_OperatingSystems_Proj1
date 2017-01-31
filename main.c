98cb99a1c80a34ca269b6119910a409828302725
rpd4g5 <rpd4g5@mail.umsl.edu>
2017-01-31 13:17:11 -0600

    Setting up Assignment 1 #6daysleftTILL-thisIs-DUE!

--- main.c
+++ main.c
@@ -1,12 +1,79 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "osProj1-log.h.h"
+
+typedef struct list_struct {
+    data_t item;
+    struct list_struct *next;
+} log_t;
+
+static log_t* headptr = NULL;
+static log_t* tailptr = NULL;
+
+int addmsg(data_t data) {
+
+    /*
+     * If successful, addmsg will return 0.
+     *
+     * If unsuccessful, addmsg will return -1
+     *
+     * Sets errno on failure
+     */
+
+    return 0;
+}
+
+void clearlog(void) {
+/*
+ * Releases all the storage that has been allocated for the logged messages
+ * and empties the list of logged messages
+ */
+
+}
+
+char* getlog(void) {
+/*
+ * Allocates enough space for a string containing the entire log, copies that
+ * log into this string, and returns a pointer to the string
+ *
+ * It is the responsibility of the calling program to free this memory when necessary
+ *
+ * A successful getlog function returns a pointer to the log string
+ *
+ * An unsuccessful getlog call returns NULL
+ *
+ * Sets errno on failure
+ */
+
+    return NULL;
+}
+
+
+int savelog(char* filename) {
+/*
+ * saves the logged messages to a disk file
+ *
+ * If successful, savelog will return 0.
+ *
+ * If unsuccessful, savelog will return -1
+ *
+ * Sets errno on failure
+ */
+
+    return 0;
+}
 
 int main() {
 
     printf("Hello, World!\n");
 
-    printf("Hello, World!\n");
+    /*
+     * The logging utility allows the caller to save
+     * a message at the end of a list (a queue that takes an array of char* pointers).
+     * The logger also records the time that the message was logged
+    */
 
-    printf("Hello, World!\n");
 
     return 0;
 }
---
+++ osProj1-log.h.h
@@ -0,0 +1,21 @@
+//
+// Created by Ryan Patrick Davis on 1/31/17.
+//
+
+#ifndef CS4760_OPERATINGSYSTEMS_PROJ1_OSPROJ1_LOG_H_H
+#define CS4760_OPERATINGSYSTEMS_PROJ1_OSPROJ1_LOG_H_H
+
+#include <time.h>
+
+typedef struct data_struct {
+    time_t time;
+    char* string;
+} data_t;
+
+int addmsg(data_t data);
+void clearlog(void);
+char* getlog(void);
+int savelog(char* filename);
+
+
+#endif //CS4760_OPERATINGSYSTEMS_PROJ1_OSPROJ1_LOG_H_H
